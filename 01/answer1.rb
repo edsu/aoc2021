e6a925893c271eecabda325849c03a58f3b4e667
Ed Summers <ehs@pobox.com>
2021-12-02 14:30:39 -0500

    thanks for the tips @mjgiarlo

--- 01/answer1.rb
+++ 01/answer1.rb
@@ -4,13 +4,11 @@ prev = nil
 larger = 0
 
 File.foreach('input') do |line| 
-  curr = Integer(line)
-  if prev != nil and prev < curr
+  curr = line.to_i
+  if prev != nil && prev < curr
     larger += 1
   end
   prev = curr
 end
 
 puts larger
-  
-
--- 01/answer2.rb
+++ 01/answer2.rb
@@ -4,7 +4,7 @@ window = []
 larger = 0
 
 File.foreach('input') do |line| 
-  window.push(Integer(line))
+  window.push(line.to_i)
   if window.length == 4
     if window[0..2].sum < window[1..3].sum
       larger += 1
